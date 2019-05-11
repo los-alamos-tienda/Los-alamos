cd62b1ab87f83c04c620ce0fb486782be2e0a4d9
ruth <naturexs58@gmail.com>
2019-05-10 22:47:10 -0400

    panel home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
+import { PrincipalComponent } from './home/principal/principal.component';
 
 @NgModule({
   declarations: [AppComponent],
