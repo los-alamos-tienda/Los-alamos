b0ae541f2b412f69466c2c533fcef957d8da81bd
Paola Cespedes <any.cespedes8@gmail.com>
2019-05-12 12:53:44 -0400

    barra de menu

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -5,9 +5,10 @@ import { RouterModule } from '@angular/router';
 import { SidenavDirective } from './navbar/sidenav.directive';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { MenuPrincipalComponent } from './menu-principal/menu-principal.component';
 
 @NgModule({
-  declarations: [NavbarComponent, FooterComponent, SidenavDirective],
+  declarations: [NavbarComponent, FooterComponent, SidenavDirective, MenuPrincipalComponent],
   imports: [CommonModule, RouterModule],
   exports: [NavbarComponent, FooterComponent]
 })
