e9071f4e80a07911c7751011a44112b17b83c22c
klaptop <klaptop@DESKTOP-N7HS39O>
2019-05-16 17:37:13 -0400

    Cambios y enrutamiento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
-import { ListaDatosComponent } from './lista-datos/lista-datos.component';
 import { MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
 import { ListaComponent } from './lista/lista.component';
 
 @NgModule({
-  declarations: [AppComponent, ListaDatosComponent, ListaComponent],
+  declarations: [AppComponent, ListaComponent],
   imports: [BrowserModule, AppRoutingModule, LayoutModule, MatTableModule, MatPaginatorModule, MatSortModule],
   bootstrap: [AppComponent]
 })
